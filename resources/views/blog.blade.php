faa70e651b8dddd0882337e1c2f5cc3576dbe745
crojaspe18dw <crojaspe18dw@ikzubirimanteo.com>
2019-10-30 08:56:28 +0100

    primer commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>blog</title>
+</head>
+<body>
+	<?php echo "este es el articulo numero  $id"; ?> 
+
+</body>
+</html>
---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>contacto</title>
+</head>
+<body>
+	Andres<br>
+	Rojas Perez<br>
+	TEL: 8392478973489<br>
+	Localidad: San Sebastian<br>
+	Sexo:M<br>
+	Idioma:Español
+
+</body>
+</html>
